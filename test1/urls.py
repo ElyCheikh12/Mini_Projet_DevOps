e7348e3386c698cc55dfd046175c40e840fa7149
23002su <23002@supnum.mr>
2025-01-29 19:45:49 +0100

    added update

--- test1/urls.py
+++ test1/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns=[
    path('' , views.seeAll , name="seeAll"),
    path('add/' , views.add , name="add"),
+   path('update/<int:id>/', views.update, name='update'),
 ]
